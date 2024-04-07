9e45cdfb859b50492019127bbd8ef80814a7264a
AlcivanLucas <alcivan14lucas@gmail.com>
2024-04-07 14:47:12 -0300

    a

--- src/app.js
+++ src/app.js
@@ -15,10 +15,10 @@ try {
         webVersionCache: { 
             type: 'remote', 
             remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', 
-        },
-        puppeteer: {
-            executablePath: '/usr/bin/google-chrome-stable',
         }
+        // puppeteer: {
+        //     executablePath: '/usr/bin/google-chrome-stable',
+        // }
     });
 
     const qrcode = require('qrcode-terminal');
